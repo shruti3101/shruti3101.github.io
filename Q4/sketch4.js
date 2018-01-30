1b934b3350e5030ca8a7e9f5f36581c46275dd6d
shruti3101 <32306077+shruti3101@users.noreply.github.com>
2018-01-29 21:56:08 -0500

    Query 4

---
+++ Q4/sketch4.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(640, 480);
+
+}
+
+function draw(){
+
+  background(255);
+
+  var h=hour();
+  var m=minute();
+  var s=second();
+
+  var angle=map(s,0,60,0,2*PI)
+
+  translate(width/2,height/2)
+  rotate(angle)
+  stroke(10)
+  line(0,0,50,0)
+
+
+}
